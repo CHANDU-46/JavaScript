a95ff890c4913a2f67240b9bf5122ef8cf16fd4a
CHANDAN R <chandan1104@icloud.com>
2025-07-05 08:21:47 +0000

    DATAYPES DONE

---
+++ BASICS/DATATYPES.js
@@ -0,0 +1,38 @@
+console.log("DATA TYPES");
+//JavaScript is Dynamically typed language you no need to depecify the DATA types
+//7  data type sin javascript
+//1.number
+let a=10;
+let b=20;
+console.log(a+b);
+//2.string
+let name1="CHANDU ";
+let name2="R";
+console.log(name1+name2);
+//3.boolean
+let isavailable=true;
+let isnotavailable=false;
+console.log(isavailable);
+console.log(isnotavailable);
+//4.undedfiend
+let x;
+console.log(x);
+//5.null
+let y= null;
+console.log(y);
+//6.Symbol
+let g=Symbol("$");
+console.log(g);
+//7.BigInt
+let z=BigInt(12345678934567);
+console.log(z);
+
+//8.Object
+console.log(typeof a);
+console.log(typeof "CHANDU");
+console.log(typeof isavailable);
+console.log(typeof x);
+console.log(typeof y);
+console.log(typeof g);
+console.log(typeof z);
+
