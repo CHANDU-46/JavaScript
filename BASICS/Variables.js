9e41d25ff26406fc3063e724e3687729a99fba38
CHANDAN R <chandan1104@icloud.com>
2025-07-05 05:29:49 +0000

    Var done

---
+++ BASICS/Variables.js
@@ -0,0 +1,18 @@
+const acountId=12234;
+console.log(acountId);
+
+//Cannot chnage the value of a variable if it once declared with const
+//acountId=2334;
+//console.log(acountId);
+
+//VARIBESL CAN BE DECLERED IN TWO WAYS Let,var
+let names = "CHANDAN R";
+var city= "BDVT";
+const mail="CHANDANA@gmail.com";
+console.log(names)
+console.log(city);
+console.log(mail);
+//VALUES CAN BE CHANGED AFTER AASIGNING TO LET,VAR
+
+//THESE VALUES CAN BE STORED IN TABLES ALSO
+console.table([acountId,names,mail,city]);
